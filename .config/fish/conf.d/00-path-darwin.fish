6e7d3a6ea9451c0458159852c95696220e370a84
Dmitry Gerasimov <di.gerasimov@gmail.com>
2024-04-28 16:53:45 +0200

    Update python3 path on macOS

--- .config/fish/conf.d/00-path-darwin.fish
+++ .config/fish/conf.d/00-path-darwin.fish
@@ -1,7 +1,7 @@
 if status is-interactive; and test (uname) = Darwin
     set PATH $PATH \
         $HOME/Library/Android/sdk/platform-tools \
-        $HOME/Library/Python/3.8/bin
+        $HOME/Library/Python/3.9/bin
 
     if test -x /opt/homebrew/bin/brew
         eval (/opt/homebrew/bin/brew shellenv)
