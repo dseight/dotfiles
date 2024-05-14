07f60c644c14ba2221c1df1c621bb833d912a5ee
Dmitry Gerasimov <di.gerasimov@gmail.com>
2024-05-14 19:35:23 +0200

    Fix jobs prompt on Linux
    jobs command has the following columns on macOS:
    
        Job Group State Command
    
    and these on Linux:
    
        Job Group CPU State Command
    
    So, while everything worked fine on macOS, on Linux it was totally
    broken.

--- .config/fish/functions/fish_jobs_prompt.fish
+++ .config/fish/functions/fish_jobs_prompt.fish
@@ -1,6 +1,6 @@
 function fish_jobs_prompt --description 'Show background jobs in prompt'
     if jobs -q
-        set -l jobs_status (jobs | string split -f4 \t | string split -f1 ' ' | string join \|)
+        set -l jobs_status (jobs -c | string join \|)
         echo -n -s (set_color magenta) " [$jobs_status]" (set_color normal)
     end
 end
