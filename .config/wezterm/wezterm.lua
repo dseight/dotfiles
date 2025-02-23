ff5aa381da56feb674f895cf65fb3076eee9fb6e
Dmitry Gerasimov <di.gerasimov@gmail.com>
2025-02-23 18:46:34 +0100

    Disable audible bell

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -36,6 +36,7 @@ config.color_scheme = scheme_for_appearance(get_appearance())
 config.hide_tab_bar_if_only_one_tab = true
 config.native_macos_fullscreen_mode = true
 config.adjust_window_size_when_changing_font_size = false
+config.audible_bell = 'Disabled'
 
 -- Navigation
 
