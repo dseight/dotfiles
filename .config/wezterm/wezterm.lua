d50f5dc93b94bb47377b99ed95b4dcb7d2583376
Dmitry Gerasimov <di.gerasimov@gmail.com>
2025-02-23 13:08:49 +0100

    Auto switch dark/light wezterm colorscheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,9 +13,26 @@ end
 
 -- Style
 
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+    if wezterm.gui then
+        return wezterm.gui.get_appearance()
+    end
+    return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+    if appearance:find 'Dark' then
+        return 'PaulMillrTweaked'
+    else
+        return 'PaulMillrTweakedLight'
+    end
+end
+
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 12.0
-config.color_scheme = 'PaulMillrTweaked'
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.hide_tab_bar_if_only_one_tab = true
 config.native_macos_fullscreen_mode = true
 config.adjust_window_size_when_changing_font_size = false
